d87df2fc30a11b7cda5cb6aa8802940cc5e760a3
tw.eqsl-server <alpine67@live.fr>
2026-02-15 09:00:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -261,16 +261,18 @@ const panelSVG = `
   <!-- séparation -->
   <line x1="20" y1="110" x2="${panelWidth - 20}" y2="110" stroke="#ccc"/>
 
-  <!-- infos QSO -->
-  <text x="20" y="150" font-size="24">Date : ${escapeXml(date)}</text>
-  <text x="20" y="185" font-size="24">UTC : ${escapeXml(time)}</text>
-  <text x="20" y="220" font-size="24">Bande : ${escapeXml(band)}</text>
-  <text x="20" y="255" font-size="24">Mode : ${escapeXml(mode)}</text>
-  <text x="20" y="290" font-size="24">Report : ${escapeXml(report)}</text>
+ <!-- infos QSO -->
+<text x="20" y="160" font-size="32" font-weight="bold">Date : ${escapeXml(date)}</text>
+<text x="20" y="205" font-size="32" font-weight="bold">UTC : ${escapeXml(time)}</text>
+<text x="20" y="250" font-size="32" font-weight="bold">Bande : ${escapeXml(band)}</text>
+<text x="20" y="295" font-size="32" font-weight="bold">Mode : ${escapeXml(mode)}</text>
+<text x="20" y="340" font-size="32" font-weight="bold">Report : ${escapeXml(report)}</text>
+
 
   <!-- note -->
   <line x1="20" y1="320" x2="${panelWidth - 20}" y2="320" stroke="#ccc"/>
-  <text x="20" y="340" font-size="22">
+ <text x="20" y="380" font-size="26">
+
   ${noteSVG}
 </text>
 
